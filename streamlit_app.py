a7c7213d153aac6ecf0159e3d08436955477154a
adampastikasamas <adampastikasamas@gmail.com>
2026-05-22 14:14:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write(
 )
 import streamlit as st
 
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("_Gabung Bang Jali :blue[pasti cuan] :sunglasses:")
 st.header("This is a header with a divider", divider="gray")
 st.header("These headers have rotating dividers", divider=True)
 st.header("One", divider=True)
